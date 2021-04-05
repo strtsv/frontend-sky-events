553ad0fab85ce69406fc98b41d48faf786d781de
jsusilo <enr@bk.ru>
2021-04-05 21:55:01 +0500

    OurWork  home component

---
+++ src/components/Home/OurWork.js
@@ -0,0 +1,285 @@
+import React from "react";
+
+import i1 from "../../assets/images/fullwidth-gallery-1-359x328.jpg";
+import i2 from "../../assets/images/fullwidth-gallery-1-359x328.jpg";
+import i3 from "../../assets/images/fullwidth-gallery-2-359x328.jpg";
+import i4 from "../../assets/images/fullwidth-gallery-2-359x328.jpg";
+import i5 from "../../assets/images/fullwidth-gallery-3-359x328.jpg";
+import i6 from "../../assets/images/fullwidth-gallery-3-359x328.jpg";
+import i7 from "../../assets/images/fullwidth-gallery-4-359x328.jpg";
+import i8 from "../../assets/images/fullwidth-gallery-4-359x328.jpg";
+import i9 from "../../assets/images/fullwidth-gallery-5-359x328.jpg";
+import i10 from "../../assets/images/fullwidth-gallery-5-359x328.jpg";
+import i11 from "../../assets/images/fullwidth-gallery-6-359x328.jpg";
+import i12 from "../../assets/images/fullwidth-gallery-6-359x328.jpg";
+import i13 from "../../assets/images/fullwidth-gallery-7-359x328.jpg";
+import i14 from "../../assets/images/fullwidth-gallery-7-359x328.jpg";
+import i15 from "../../assets/images/fullwidth-gallery-8-359x328.jpg";
+import i16 from "../../assets/images/fullwidth-gallery-8-359x328.jpg";
+
+class OurWork extends React.Component {
+  render() {
+    return (
+      <section className="section section-md section-fluid section-bottom-55">
+        <div className="container-fluid isotope-wrap">
+          <h2
+            className="text-uppercase text-center wow fadeInUp"
+            data-wow-delay=".2s"
+          >
+            Our Gallery
+          </h2>
+          <div
+            className="row isotope no-gutters"
+            data-isotope-layout="fitRows"
+            data-isotope-group="gallery"
+            data-lightgallery="group"
+          >
+            <div
+              className="col-12 col-sm-6 col-lg-4 col-xl-3 isotope-item wow fadeInLeft"
+              data-filter="Type 3"
+            >
+              {}
+              <article className="thumbnail thumbnail-classic thumbnail-md">
+                <div className="thumbnail-classic-figure">
+                  <img src={i1} alt width={359} height={328} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div className="thumbnail-classic-title-wrap">
+                    <a
+                      className="icon fl-bigmug-line-zoom60"
+                      href="images/fullwidth-gallery-1-1200x800-original.jpg"
+                      data-lightgallery="item"
+                    >
+                      <img src={i2} alt width={359} height={328} />
+                    </a>
+                    <h3 className="thumbnail-classic-title text-uppercase">
+                      <a href="single-portfolio.html">iStep Corporate Event</a>
+                    </h3>
+                  </div>
+                  <p className="thumbnail-classic-text text-center text-white">
+                    April 2018
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div
+              className="col-12 col-sm-6 col-lg-4 col-xl-3 isotope-item wow fadeInLeft"
+              data-filter="Type 2"
+              data-wow-delay=".1s"
+            >
+              {}
+              <article className="thumbnail thumbnail-classic thumbnail-md">
+                <div className="thumbnail-classic-figure">
+                  <img src={i3} alt width={359} height={328} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div className="thumbnail-classic-title-wrap">
+                    <a
+                      className="icon fl-bigmug-line-zoom60"
+                      href="images/fullwidth-gallery-2-1200x800-original.jpg"
+                      data-lightgallery="item"
+                    >
+                      <img src={i4} alt width={359} height={328} />
+                    </a>
+                    <h3 className="thumbnail-classic-title text-uppercase">
+                      <a href="single-portfolio.html">GMC Anniversary</a>
+                    </h3>
+                  </div>
+                  <p className="thumbnail-classic-text text-center text-white">
+                    March 2018
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div
+              className="col-12 col-sm-6 col-lg-4 col-xl-3 isotope-item wow fadeInLeft"
+              data-filter="Type 1"
+              data-wow-delay=".2s"
+            >
+              {}
+              <article className="thumbnail thumbnail-classic thumbnail-md">
+                <div className="thumbnail-classic-figure">
+                  <img src={i5} alt width={359} height={328} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div className="thumbnail-classic-title-wrap">
+                    <a
+                      className="icon fl-bigmug-line-zoom60"
+                      href="images/grid-gallery-1-1200x800-original.jpg"
+                      data-lightgallery="item"
+                    >
+                      <img src={i6} alt width={359} height={328} />
+                    </a>
+                    <h3 className="thumbnail-classic-title text-uppercase">
+                      <a href="single-portfolio.html">Wake Co. Masterclass</a>
+                    </h3>
+                  </div>
+                  <p className="thumbnail-classic-text text-center text-white">
+                    February 2018{" "}
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div
+              className="col-12 col-sm-6 col-lg-4 col-xl-3 isotope-item wow fadeInLeft"
+              data-filter="Type 3"
+              data-wow-delay=".3s"
+            >
+              {}
+              <article className="thumbnail thumbnail-classic thumbnail-md">
+                <div className="thumbnail-classic-figure">
+                  <img src={i7} alt width={359} height={328} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div className="thumbnail-classic-title-wrap">
+                    <a
+                      className="icon fl-bigmug-line-zoom60"
+                      href="images/fullwidth-gallery-4-1200x800-original.jpg"
+                      data-lightgallery="item"
+                    >
+                      <img src={i8} alt width={359} height={328} />
+                    </a>
+                    <h3 className="thumbnail-classic-title text-uppercase">
+                      <a href="single-portfolio.html">Eventy Conference</a>
+                    </h3>
+                  </div>
+                  <p className="thumbnail-classic-text text-center text-white">
+                    January 2018
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div
+              className="col-12 col-sm-6 col-lg-4 col-xl-3 isotope-item wow fadeInRight"
+              data-filter="Type 2"
+            >
+              {}
+              <article className="thumbnail thumbnail-classic thumbnail-md">
+                <div className="thumbnail-classic-figure">
+                  <img src={i9} alt width={359} height={328} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div className="thumbnail-classic-title-wrap">
+                    <a
+                      className="icon fl-bigmug-line-zoom60"
+                      href="images/fullwidth-gallery-5-1200x800-original.jpg"
+                      data-lightgallery="item"
+                    >
+                      <img src={i10} alt width={359} height={328} />
+                    </a>
+                    <h3 className="thumbnail-classic-title text-uppercase">
+                      <a href="single-portfolio.html">Techno Workshop</a>
+                    </h3>
+                  </div>
+                  <p className="thumbnail-classic-text text-center text-white">
+                    December 2017
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div
+              className="col-12 col-sm-6 col-lg-4 col-xl-3 isotope-item wow fadeInRight"
+              data-filter="Type 1"
+              data-wow-delay=".1s"
+            >
+              {}
+              <article className="thumbnail thumbnail-classic thumbnail-md">
+                <div className="thumbnail-classic-figure">
+                  <img src={i11} alt width={359} height={328} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div className="thumbnail-classic-title-wrap">
+                    <a
+                      className="icon fl-bigmug-line-zoom60"
+                      href="images/fullwidth-gallery-6-1200x800-original.jpg"
+                      data-lightgallery="item"
+                    >
+                      <img src={i12} alt width={359} height={328} />
+                    </a>
+                    <h3 className="thumbnail-classic-title text-uppercase">
+                      <a href="single-portfolio.html">Hearst Center Opening</a>
+                    </h3>
+                  </div>
+                  <p className="thumbnail-classic-text text-center text-white">
+                    November 2017
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div
+              className="col-12 col-sm-6 col-lg-4 col-xl-3 isotope-item wow fadeInRight"
+              data-filter="Type 3"
+              data-wow-delay=".2s"
+            >
+              {}
+              <article className="thumbnail thumbnail-classic thumbnail-md">
+                <div className="thumbnail-classic-figure">
+                  <img src={i13} alt width={359} height={328} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div className="thumbnail-classic-title-wrap">
+                    <a
+                      className="icon fl-bigmug-line-zoom60"
+                      href="images/fullwidth-gallery-7-1200x800-original.jpg"
+                      data-lightgallery="item"
+                    >
+                      <img src={i14} alt width={359} height={328} />
+                    </a>
+                    <h3 className="thumbnail-classic-title text-uppercase">
+                      <a href="single-portfolio.html">MDate Meeting</a>
+                    </h3>
+                  </div>
+                  <p className="thumbnail-classic-text text-center text-white">
+                    October 2017
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div
+              className="col-12 col-sm-6 col-lg-4 col-xl-3 isotope-item wow fadeInRight"
+              data-filter="Type 2"
+              data-wow-delay=".3s"
+            >
+              {}
+              <article className="thumbnail thumbnail-classic thumbnail-md">
+                <div className="thumbnail-classic-figure">
+                  <img src={i15} alt width={359} height={328} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div className="thumbnail-classic-title-wrap">
+                    <a
+                      className="icon fl-bigmug-line-zoom60"
+                      href="images/fullwidth-gallery-8-1200x800-original.jpg"
+                      data-lightgallery="item"
+                    >
+                      <img src={i16} alt width={359} height={328} />
+                    </a>
+                    <h3 className="thumbnail-classic-title text-uppercase">
+                      <a href="single-portfolio.html">SubCo Global Event</a>
+                    </h3>
+                  </div>
+                  <p className="thumbnail-classic-text text-center text-white">
+                    September 2017
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div className="col-1" />
+            <div className="button-style-2">
+              <a
+                className="wow fadeInDown button button-lg button-icon button-icon-left button-third button-ujarak"
+                data-wow-delay=".2s"
+                href="portfolio.html"
+              >
+                <span className="icon mdi mdi-plus" />
+                Learn more
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default OurWork;
