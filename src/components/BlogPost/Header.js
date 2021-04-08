a786a4245b57ef2350952bd98e04f8f7571b6eda
jsusilo <enr@bk.ru>
2021-04-08 19:08:38 +0500

    PostBlog header component

---
+++ src/components/BlogPost/Header.js
@@ -0,0 +1,269 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="section page-header">
+        {}
+        <div className="rd-navbar-wrap">
+          <nav
+            className="rd-navbar rd-navbar-classic"
+            data-layout="rd-navbar-fixed"
+            data-sm-layout="rd-navbar-fixed"
+            data-md-layout="rd-navbar-fixed"
+            data-md-device-layout="rd-navbar-fixed"
+            data-lg-layout="rd-navbar-static"
+            data-lg-device-layout="rd-navbar-static"
+            data-xl-layout="rd-navbar-static"
+            data-xl-device-layout="rd-navbar-static"
+            data-lg-stick-up-offset="46px"
+            data-xl-stick-up-offset="46px"
+            data-xxl-stick-up-offset="46px"
+            data-lg-stick-up="true"
+            data-xl-stick-up="true"
+            data-xxl-stick-up="true"
+          >
+            <div className="rd-navbar-main-outer">
+              <div className="rd-navbar-main">
+                {}
+                <div className="rd-navbar-panel">
+                  {}
+                  <button
+                    className="rd-navbar-toggle"
+                    data-rd-navbar-toggle=".rd-navbar-nav-wrap"
+                  >
+                    <span />
+                  </button>
+                  {}
+                  <div className="rd-navbar-brand">
+                    <a className="brand" href="index.html">
+                      <img
+                        className="brand-logo-dark"
+                        src="images/logo-default-119x44.png"
+                        alt
+                        width={119}
+                        height={44}
+                        srcSet="images/logo-default-119x44.png 2x"
+                      />
+                      <img
+                        className="brand-logo-light"
+                        src="images/logo-default-119x44.png"
+                        alt
+                        width={119}
+                        height={44}
+                        srcSet="images/logo-default-119x44.png 2x"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="rd-navbar-main-element">
+                  <div className="rd-navbar-nav-wrap">
+                    <ul className="rd-navbar-nav">
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="index.html">
+                          Home
+                        </a>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="about.html">
+                          About
+                        </a>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="portfolio.html">
+                          Portfolio
+                        </a>
+                        <ul className="rd-menu rd-navbar-dropdown">
+                          <li className="rd-dropdown-item">
+                            <a
+                              className="rd-dropdown-link"
+                              href="single-portfolio.html"
+                            >
+                              Single Portfolio
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="services.html">
+                          Services
+                        </a>
+                        <ul className="rd-menu rd-navbar-dropdown">
+                          <li className="rd-dropdown-item">
+                            <a
+                              className="rd-dropdown-link"
+                              href="single-service.html"
+                            >
+                              Single Service
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li className="rd-nav-item active">
+                        <a className="rd-nav-link" href="classic-blog.html">
+                          Blog
+                        </a>
+                        <ul className="rd-menu rd-navbar-dropdown">
+                          <li className="rd-dropdown-item">
+                            <a
+                              className="rd-dropdown-link"
+                              href="classic-blog.html"
+                            >
+                              Classic Blog
+                            </a>
+                          </li>
+                          <li className="rd-dropdown-item">
+                            <a
+                              className="rd-dropdown-link"
+                              href="modern-blog.html"
+                            >
+                              Modern Blog
+                            </a>
+                          </li>
+                          <li className="rd-dropdown-item active">
+                            <a
+                              className="rd-dropdown-link"
+                              href="blog-post.html"
+                            >
+                              Blog Post
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="#">
+                          Pages
+                        </a>
+                        <ul className="rd-menu rd-navbar-megamenu">
+                          <li className="rd-megamenu-item">
+                            <ul className="rd-megamenu-list">
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="typography.html"
+                                >
+                                  Typography
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="buttons.html"
+                                >
+                                  Buttons
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="forms.html"
+                                >
+                                  Forms
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="tabs-and-accordions.html"
+                                >
+                                  Tabs and Accordions
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="progress-bars.html"
+                                >
+                                  Progress Bars
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="tables.html"
+                                >
+                                  Tables
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="grid-system.html"
+                                >
+                                  Grid System
+                                </a>
+                              </li>
+                            </ul>
+                          </li>
+                          <li className="rd-megamenu-item">
+                            <ul className="rd-megamenu-list">
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="404-page.html"
+                                >
+                                  404 Page
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="503-page.html"
+                                >
+                                  503 Page
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="under-construction.html"
+                                >
+                                  Under Construction
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="coming-soon.html"
+                                >
+                                  Coming Soon
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="privacy-policy.html"
+                                >
+                                  Privacy Policy
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="search-results.html"
+                                >
+                                  Search Results
+                                </a>
+                              </li>
+                            </ul>
+                          </li>
+                        </ul>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="contacts.html">
+                          Contacts
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
