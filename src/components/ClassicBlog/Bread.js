425aa39d7acc88b0bea05f69ab52e70e200570ac
jsusilo <enr@bk.ru>
2021-04-08 19:07:29 +0500

    ClassicBlog bread component

---
+++ src/components/ClassicBlog/Bread.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import i from "../../assets/images/bg-about-01-1700x376.jpg";
+
+class Bread extends React.Component {
+  render() {
+    return (
+      <section
+        className="section-xl context-dark bg-image-2"
+        style={{
+          backgroundImage: "url(" + i + ")",
+        }}
+      >
+        <div className="container">
+          <div className="row justify-content-center text-center">
+            <div className="col-lg-12">
+              <h2 className="text-uppercase text-center">Classic Blog</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Bread;
